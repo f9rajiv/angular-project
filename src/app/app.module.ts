02abf9d3e0e41fc7146f9b951b1e50e8d3788e7c
f9rajiv <f9rajiv@gamil.com>
2020-05-08 02:38:32 +0545

    componet grid and card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import {MatListModule} from "@angular/material/list"
+import {MatGridListModule} from "@angular/material/grid-list"
+import {MatCardModule} from "@angular/material/card"
+import {MatButtonModule} from "@angular/material/button"
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,9 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
     
   
   ],
