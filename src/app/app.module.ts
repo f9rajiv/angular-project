95b60d40a85d746fd2c5eff85c0e2d02279b2d13
f9rajiv <f9rajiv@gamil.com>
2020-05-16 02:41:26 +0545

    angular routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,29 @@ import {MatGridListModule} from "@angular/material/grid-list"
 import {MatCardModule} from "@angular/material/card"
 import {MatButtonModule} from "@angular/material/button"
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component'
 import { MenuComponent } from './menu/menu.component';
-import { from } from 'rxjs';
-
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {DishService} from "./services/dish.service";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +40,8 @@ import { FooterComponent } from './footer/footer.component'
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
     
   
   ],
