e04cc2de4144823a125e1f4c7ae0507d1f185023
f9rajiv <f9rajiv@gamil.com>
2020-05-21 14:22:29 +0545

    promise(pending)

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Dish } from "../shared/dish";
 import {Params,ActivatedRoute} from "@angular/router";
 import{Location} from '@angular/common';
 import{DishService} from '../services/dish.service';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -24,7 +25,8 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     let id =this.route.snapshot.params['id'];
-  this.dish =this.dishService.getDish(id);
+ this.dishService.getDish(id)
+  .then((dish)=>this.dish=dish);
   }
 goBack():void{
   this.location.back();
