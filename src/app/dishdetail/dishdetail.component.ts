429fe8f8fd219ff8397d440251597261876ed088
f9rajiv <f9rajiv@gamil.com>
2020-05-27 23:39:30 +0545

    saving in server

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,7 +20,7 @@ export class DishdetailComponent implements OnInit {
   
   commentForm: FormGroup;
   comment: Comment;
-
+  dishcopy: Dish;
   errMess: string;
     dish:Dish;
     dishIds:string[];
@@ -59,7 +59,7 @@ export class DishdetailComponent implements OnInit {
   .subscribe((dishIds)=> this.dishIds = dishIds);
  this.route.params.pipe(switchMap((params:Params)=>this.dishService.getDish(params['id'])))
 
-  .subscribe((dish)=>{this.dish=dish; this.setPrevNext(dish.id);},
+  .subscribe((dish)=>{this.dish=dish;  this.dishcopy = dish; this.setPrevNext(dish.id);},
   errmess => this.errMess = <any>errmess);
   }
 setPrevNext(dishId:string)
@@ -109,7 +109,12 @@ onValueChanged(data?:any)
 onSubmit() {
   this.comment = this.commentForm.value;
   this.comment.date = new Date().toISOString();
-  this.dish.comments.push(this.comment);
+  this.dishcopy.comments.push(this.comment);
+  this.dishService.putDish(this.dishcopy)
+  .subscribe(dish => {
+    this.dish = dish; this.dishcopy = dish;
+  },
+  errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
   console.log(this.comment);
   this.comment = null;
   this.commentForm.reset({
